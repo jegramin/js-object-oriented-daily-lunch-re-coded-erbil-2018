d22f2640c8d7cc163333708cc1ed414508825152
Mohammed Ameen <jegroamin@gmail.com>
2018-09-11 21:23:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,10 +54,10 @@ class Customer{
       return delivery.customerId === this.id; 
     });
   }
-  meals(){
-    return store.meals.filter(meal =>{
-      return meal.customerId === this.id;
-    });
+  meals() {
+      return this.deliveries().map(delivery => delivery.meal());
+    }
+
   }
   totalSpent(){
     return this.meals().reduce((total,meal) => (total += meal.price),0)
