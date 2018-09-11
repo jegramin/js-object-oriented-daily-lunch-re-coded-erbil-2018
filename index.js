2a0ad82fc260aa7ecbffa915a4dbd6ef20e609b6
Mohammed Ameen <jegroamin@gmail.com>
2018-09-11 20:31:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,81 @@
 // global datastore
 let store = { neighborhoods: [], meals: [], customers: [], deliveries: [] };
+
+let neighborhoodId = 0;
+class Neighborhood{
+  constructor(name){
+    this.name = name;
+    this.id = ++neighborhoodId;
+    store.neighborhoods.push(this);
+  }
+  
+  deliveries(){
+    return store.deliveries;
+  }
+  customers(){
+    return store.customers;
+  }
+  meals(){
+    return store.meals;
+  }
+}
+
+let mealId=0
+class Meal{
+  constructor(title,price){
+    this.title = title;
+    this.price = price;
+    this.id = ++mealId;
+    store.meals.push(this);
+}
+ deliveries(){
+    return store.deliveries.filter(delivery => {
+      return delivery.mealId === this.id; 
+    });
+  }
+  customers(){
+    return store.customers;
+  }
+}
+
+let customerId = 0;
+class Customer{
+  constructor(name){
+    this.name = name;
+    this.id = ++customerId;
+    this.neighborhoodId = this.id;
+    store.customers.push(this);
+  }
+  deliveries(){
+    return store.deliveries.filter(delivery => {
+      return delivery.customerId === this.id; 
+    });
+  }
+  meals(){
+    return store.meals.filter(meal =>{
+      return meal.customerId = this.id;
+    });
+  }
+}
+
+
+let deliveryId = 0;
+class Delivery{
+   constructor(mealId,neighborhoodId,customerId){
+    this.mealId = mealId;
+    this.neighborhoodId = neighborhoodId;
+    this.customerId = customerId;
+    this.id = ++deliveryId;
+    store.deliveries.push(this);
+}
+ neighborhood(){
+    return store.neighborhoods[0];
+  }
+  customer(){
+    return store.customers[0];
+  }
+  meal(){
+    return store.meals[0];
+  }
+}
+
