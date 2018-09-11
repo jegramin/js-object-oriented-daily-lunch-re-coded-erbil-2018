be9cb78a0eed7c18eaf00c94b0451f39248691a2
Mohammed Ameen <jegroamin@gmail.com>
2018-09-11 21:15:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ class Customer{
   }
   meals(){
     return store.meals.filter(meal =>{
-      return meal.deliveryId === this.customerId;
+      return meal.mealId === this.customerId;
     });
   }
   totalSpent(){
